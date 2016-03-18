ab76d42181e26a03ee9bf31431ca088bbc870952
acarrara <carrara@esteco.com>
2016-03-18 15:44:00 +0100

    heroku working

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/app'));
 var server = app.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
 });
