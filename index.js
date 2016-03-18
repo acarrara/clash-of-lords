ca0fbe990af0f66fa9b51744411c5a8cad94d558
acarrara <carrara@esteco.com>
2016-03-18 15:07:07 +0100

    Procfile added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = process.env.PORT || 5000;
-connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log('Server running on ' + port + '...');
+var port = Number(process.env.PORT || 5000);
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+var server = app.listen(port, function () {
+    console.log('Listening on port %d', server.address().port);
 });
