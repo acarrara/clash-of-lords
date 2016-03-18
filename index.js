48aec3ab2c7bdc6ae333e2a2753e60029d35592f
acarrara <carrara@esteco.com>
2016-03-18 15:12:33 +0100

    Procfile added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var port = Number(process.env.PORT || 5000);
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/app'));
 var server = app.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
