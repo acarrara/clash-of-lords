3da26e30b01ad8e411581b5c2b654e824a8a3860
acarrara <carrara@esteco.com>
2016-03-18 15:21:12 +0100

    Procfile added

--- tasks/serve.js
+++ tasks/serve.js
@@ -6,6 +6,8 @@ var config = require('../gulp.config')();
 gulp.task('serve-dev', ['wiredep', 'tsc-app', 'watch-ts', 'watch-sass'], function () {
     liveServer.start(config.liveServer.dev);
 });
+gulp.task('serve-heroku', ['wiredep', 'tsc-app', 'watch-ts', 'watch-sass'], function () {
+}
 
 /* Start live server production mode */
 gulp.task('serve-build', ['build'], function () {
