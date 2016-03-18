3287354a189f9d6a0974381b8848c075047f06b0
acarrara <carrara@esteco.com>
2016-03-18 16:11:26 +0100

    Added graceful-fs

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// Make sure to read the caveat below.
+var realFs = require('fs');
+var gracefulFs = require('graceful-fs');
+gracefulFs.gracefulify(realFs);
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 var tasks = requireDir('./tasks');
