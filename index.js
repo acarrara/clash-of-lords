8ff5910a9d216e994348c817b5ef5a6a9b01f437
acarrara <carrara@esteco.com>
2016-04-01 16:18:39 +0200

    node port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var port = Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);
+var port = Number(process.env.NODE_PORT || process.env.PORT || 5000);
 
 var express = require('express');
 var app = express();
