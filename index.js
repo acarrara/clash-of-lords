f5233ea613f245d04b62ca58b90941ccd464d805
acarrara <carrara@esteco.com>
2016-04-01 14:09:51 +0200

    removed ip configuration

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var port = Number(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000);
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-var server = app.listen(port, ip, function () {
+var server = app.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
 });
